8f3fca7f40f4f9f67671b6d70d05b5dbaf1d6587
MacBook Pro <macbookpro@macbook-2012-23.local>
2015-11-11 15:29:18 -0800

    Set up bcrypt

--- model/user.js
+++ model/user.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  hash: String
 });
 
 var User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var User = require('./model/user');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');//now I can read the cookies before just setting them.
+var bcrypt = require('bcrypt-nodejs');
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/chatusersdb");
+mongoose.connect("mongodb://localhost/chatdb");
 
 app.use(express.static('public'));
 
@@ -16,11 +17,14 @@ app.use(cookieParser());
 app.get('/', function(req, res){
   var user = req.cookies.user;//var user is the same user in cookies.user //this wil evaluate to what you set cookie so if just sert it will have somethingb but if logout undefine
   if(user) {
-    res.send("Hello World," + user.username);
+    res.send("Hello World," + user);
   } else {
     res.send("Hello World, Please Login");
   }
 });
+
+
+
 //var user = req.cookies('user');
 // var username = user.username;
 
@@ -32,19 +36,25 @@ app.get('/user', function(req, res){
 });
 
 app.post('/create/user', function(req, res){
-  var user = new User(req.body);
+  var user = new User({username:req.body.username, password: bcrypt.hashSync(req.body.password)});
   user.save(function(err, user) {
-    res.cookie('user', user);
+    res.cookie('user', user.username);
     res.redirect('/');
     });
 });
 
 app.post('/login', function(req, res){ //you want to use get when your not chaging something..here we are changing state from logged out to login so its a post
-  User.findOne({username: req.body.username, password: req.body.password}, function(err, user) {
-    res.cookie('user', user);//cookies are data stored in the browser. we want store the logged in user in the cookies.
-    res.redirect('/');//this is setting the cookie with the user key 'user'
-  });//to get user data for later.
-});// '/' will always go to index by default if we have static files turned on.
+  User.findOne({username: req.body.username}, function(err, user) {
+    if(bcrypt.compareSync(req.body.password, user.password)){
+      res.cookie('user', user.username);
+      res.redirect('/');
+    };
+  });
+});
+//    res.cookie('user', user);//cookies are data stored in the browser. we want store the logged in user in the cookies.
+//    res.redirect('/');//this is setting the cookie with the user key 'user'
+//to get user data for later.
+// '/' will always go to index by default if we have static files turned on.
 //We want the site to remember if someone is logged in with cookies. We deposit a cookie into the browser by using res.cookie. res.cookie is built into express
 
 app.post('/logout', function(req, res){
@@ -52,6 +62,9 @@ app.post('/logout', function(req, res){
   res.redirect('/');
 });
 
+app.delete('/delete', function(req, res){
+  User.findAndRemove
+});
 
 app.listen(3000, function(){
   console.log('Ready on Port 3000');
@@ -60,7 +73,7 @@ app.listen(3000, function(){
 
 //2pm tomorrow
 //1.Get bcrypt
-//ecrypt user password when create user and match encrypted password in login
+//bcrypt user password when create user and match encrypted password in login
 //you need to change the create and the login.
 //you want to delete the users in there now since not set up to encrypt users
 //I dont want password in the cookie.
