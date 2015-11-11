707ca52af95791beabf240cb7c9535cd8a0a5a7a
MacBook Pro <macbookpro@macbook-2012-23.local>
2015-11-10 18:56:08 -0800

    Setup Logout

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.get('/user', function(req, res){
 
 app.post('/create/user', function(req, res){
   var user = new User(req.body);
-  user.save(function(err, users) {
+  user.save(function(err, user) {
+    res.cookie('user', user);
     res.redirect('/');
     });
 });
@@ -26,9 +27,15 @@ app.post('/create/user', function(req, res){
 app.post('/login', function(req, res){ //you want to use get when your not chaging something..here we are changing state from logged out to login so its a post
   User.findOne({username: req.body.username, password: req.body.password}, function(err, user) {
     res.cookie('user', user);//cookies are data stored in the browser. we want store the logged in user in the cookies.
-    res.redirect('/');
+    res.redirect('/');//this is setting the cookie with the user key 'user'
   });//to get user data for later.
 });// '/' will always go to index by default if we have static files turned on.
+//We want the site to remember if someone is logged in with cookies. We deposit a cookie into the browser by using res.cookie. res.cookie is built into express
+
+app.post('/logout', function(req, res){
+  res.clearCookie('user');//here it deletes the 'user' cookie
+  res.redirect('/');
+});
 
 app.listen(3000, function(){
   console.log('Ready on Port 3000');
